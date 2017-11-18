e801a45b099f0acdeeb9674f527918eb3649b315
Viacheslav <slavcheg21071993@gmail.com>
2017-11-18 23:15:56 +0200

    Update App.java

--- app/src/main/java/com/example/krasn/agent08/App.java
+++ app/src/main/java/com/example/krasn/agent08/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
-        CloudRail.setAppKey("592ac2b124c5b453109b3f07");
+        CloudRail.setAppKey("appKey");
     }
     public static Context getContext(){
         return context;
